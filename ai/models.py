d98b05312b6466fe40a0ce414bc71fe9fca034dd
meain <abinsimon10@gmail.com>
2016-02-19 19:16:51 +0530

    Initial commit - nothing funcitonal

---
+++ ai/models.py
@@ -0,0 +1,13 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+from django.utils import timezone
+
+class Querry(models.Model):
+    querry_term = models.CharField(max_length = 1000)
+    timestamp = models.DateTimeField(default = timezone.now())
+
+    def __unicode__(self):
+        return self.querry_term
