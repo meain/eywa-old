497fc79064db8a2b3998a89f34f9f5fccb17f8cb
meain <abinsimon10@gmail.com>
2016-03-06 13:05:33 +0530

    ui

--- ai/static/ai/js/chatjs.js
+++ ai/static/ai/js/chatjs.js
@@ -2,7 +2,6 @@ $(document).ready(function(){
 $(".chat-send-button").click(function(){
     var msg = $(".chat-message-text").val();
     var urlmsg = encodeURIComponent(msg);
-alert(urlmsg)
     $.getJSON('/api/'+urlmsg, function(data, jqXHR){
 $(".chat-message-text").val("")
 $("<div class = 'msg_user'>"+msg+"</div>").insertBefore(".reference");
