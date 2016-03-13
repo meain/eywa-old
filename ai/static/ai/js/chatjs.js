2b83a32061a62d311944b878f780c9e2b0a05244
meain <abinsimon10@gmail.com>
2016-03-13 16:01:42 +0530

    update

--- ai/static/ai/js/chatjs.js
+++ ai/static/ai/js/chatjs.js
@@ -1,9 +1,14 @@
 $(document).ready(function(){
-$(".chat-send-button").click(function(){
-    var msg = $(".chat-message-text").val();
+$("#chat-message-text").keyup(function(event){
+    if(event.keyCode == 13){
+        $("#chat-send-button").click();
+    }
+});
+$("#chat-send-button").click(function(){
+    var msg = $("#chat-message-text").val();
     var urlmsg = encodeURIComponent(msg);
     $.getJSON('/api/'+urlmsg, function(data, jqXHR){
-$(".chat-message-text").val("")
+$("#chat-message-text").val("")
 $("<div class = 'msg_user'>"+msg+"</div>").insertBefore(".reference");
 $("<div class = 'msg_ai'>"+data['fields']['querry_result']+"</div>").insertBefore(".reference");
 $("#chat-msg-box").scrollTop($("#chat-msg-box")[0].scrollHeight);
