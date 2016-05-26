ac4c7154093835e58b8d20c2960eacdee6fc1400
Abin Simon <abinsimon10@gmail.com>
2016-05-26 12:16:49 +0530

    Fix load

--- ai/reply.py
+++ ai/reply.py
@@ -45,8 +45,8 @@ def wolfram_result(query):
 def aiml_result(query, k):
     item = {}
     # the aiml takes time to load, will have to run it in a parallel thread
-    #item['content'] = "Not connected to aiml!"
-    item['content'] = reply_aiml(query,k)
+    item['content'] = "Not connected to aiml!"
+    # item['content'] = reply_aiml(query,k)
     item['type'] = 'text'
     return item
 
--- ai/views.py
+++ ai/views.py
@@ -31,48 +31,21 @@ from .forms import QuerryForm
 # ========================================================================================
 # ========================================================================================
 
+def load_aiml():
+    global k
+    # k = initialize_aiml()
+    print "Still loading"
+
 def index(request):
     form = QuerryForm(request.POST or None)
-    if form.is_valid():
-        instance = form.save(commit = False)
-        print instance.querry_term
-
-        #This is what you will hve to edit
-        instance.querry_result = customFuncion(instance.querry_term)
-
-        instance.save()
-        # return redirect('result', instance)
-        return render(request, "ai/result.html", {'querry' : instance})
-    else:
-        print "Not valid"
-        global k
-        # k = initialize_aiml()
     context = {'form' : form}
+    aiml_thread = threading.Thread(target=load_aiml())
+    aiml_thread.start()
     return render(request, "ai/index.html", context)
 
 class QuerryResult(generics.RetrieveUpdateDestroyAPIView):
     queryset = Querry.objects.all()
     serializer_class = AiSerializer
-
-def ajax_view(request, msg, id):
-    print "\n\n------------------------------------------------------------------"
-    print "User message : " + msg + "\nUser id : "  + id + "\n"
-    # it is currently in unicode format and has to be converted into string
-    urllib.unquote(msg).decode('utf8')
-    urllib.unquote(id).decode('utf8')
-    global k
-    q_res= get_result(msg, id, k)
-    q = Querry(querry_term = msg, querry_result = q_res, timestamp = timezone.now())
-    q.save()
-    print "Result : \n" + str(q_res)
-    data = {}
-    data['querry_term'] = q.querry_term
-    data['resultsno'] = len(q_res)
-    data['results'] = q_res
-    data = json.dumps(data)
-    print "\nJson response : \n" + data
-    print "------------------------------------------------------------------\n\n"
-    return HttpResponse(data, content_type='application/json')
 
 def reply(request):
     if request.method == 'POST':
